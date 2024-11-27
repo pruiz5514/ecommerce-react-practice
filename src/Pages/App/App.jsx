f31569495b183ebf73989e118295e60df9f3ecd3
Pablo Ruiz <pablo_ruiza@hotmail.com>
2024-11-26 22:35:21 -0500

    navegation configuration

---
+++ src/Pages/App/App.jsx
@@ -0,0 +1,32 @@
+import { useRoutes, BrowserRouter } from 'react-router-dom'
+import Home from '../Home/Home'
+import MyAccount from '../MyAccount/MyAccount'
+import MyOrder from '../MyOrder/MyOrder'
+import MyOrders from '../MyOrders/MyOrders'
+import NotFound from '../NotFound/NotFound'
+import SignIn from '../SignIn/SignIn'
+import './App.css'
+
+const AppRoutes = () => {
+  let routes = useRoutes([
+    { path: '/', element: <Home/>},
+    { path: '/my-account', element: <MyAccount/>},
+    { path: '/my-order', element: <MyOrder/>},
+    { path: '/my-orders', element: <MyOrders/>},
+    { path: '/sign-in', element: <SignIn/>},
+    { path: '/*', element: <NotFound/>},
+  ])
+
+  return routes;
+}
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <AppRoutes/>
+    </BrowserRouter>
+  )
+}
+
+export default App
